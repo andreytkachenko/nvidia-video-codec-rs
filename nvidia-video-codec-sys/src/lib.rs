949bce138ae0bc657322c8f92af333d2ea32df51
Andrey Tkachenko <a.tkachenko@kryptonite.ru>
2024-01-16 13:27:27 +0400

    bump up bindgen

--- nvidia-video-codec-sys/src/lib.rs
+++ nvidia-video-codec-sys/src/lib.rs
@@ -1,24 +1,5 @@
-// TODO do w/out the unions?
-#![feature(untagged_unions)]
+#![allow(improper_ctypes)]
 
 pub mod cuda;
 pub mod cuvid;
 pub mod nvenc;
-
-#[cfg(test)]
-mod tests {
-    use super::cuda::*;
-    #[test]
-    fn init_and_version() {
-        let ret = unsafe { cuInit(0) };
-        println!("{:?}", ret);
-
-        let ver = unsafe {
-            let mut ver = 0;
-            cuDriverGetVersion(&mut ver as *mut i32);
-            ver
-        };
-
-        println!("Version {}", ver);
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,1 @@
 extern crate nvidia_video_codec_sys as ffi;
-
-#[macro_use]
-mod macros;
-
-pub mod cuda;
